55a50e72120abe03f5ea87a5159c4864432e977d
ABDULBASITLATIF <basitlatif347@gmail.com>
2026-02-09 17:27:44 +0500

    9 feb

--- webapp/controller/NewDoc.controller.js
+++ webapp/controller/NewDoc.controller.js
@@ -5,6 +5,34 @@ sap.ui.define([
 
     return Controller.extend("zfi.payment.management.controller.NewDoc", {
         onInit() {
-        }
+        },
+        onCreate: async function (oEvent) {
+
+            var oView = this.getView();
+
+            if (!this._pTablePostingDialog) {
+                this._pTablePostingDialog = sap.ui.core.Fragment.load({
+                    id: oView.getId(),
+                    name: "zfi.payment.management.fragments.Create",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            var oDialogTP = await this._pTablePostingDialog;
+            oDialogTP.open();
+
+        },
+        onCloseCreateDialog() {
+
+            if (this._pTablePostingDialog) {
+                this._pTablePostingDialog.then(function (oDialog) {
+                    oDialog.close();
+                });
+            }
+        },
+
     });
 });
